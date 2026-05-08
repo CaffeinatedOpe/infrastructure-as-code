b3f8626036d41ed63876ddeb0e2ce1e79908c37e
CaffeinatedOpe <caffeinatedope@proton.me>
2026-05-08 10:59:41 -0500

    surely this won't ruin anything at all

---
+++ cluster/management/jenkins/seed-jobs/eng.groovy
@@ -0,0 +1,40 @@
+#!/usr/bin/env groovy
+
+pipelineJob('eng') {
+    displayName('eng site')
+
+    logRotator {
+        numToKeep(100)
+        daysToKeep(90)
+    }
+
+		properties {
+        pipelineTriggers {
+            triggers {
+                pollSCM {
+    							scmpoll_spec('H/5 * * * *')
+								} 
+            }
+        }
+    }
+
+    configure { project ->
+        project / 'properties' / 'org.jenkinsci.plugins.workflow.job.properties.DurabilityHintJobProperty' {
+            hint('PERFORMANCE_OPTIMIZED')
+        }
+    }
+
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/CaffeinatedOpe/engl-proj')
+                    }
+                    branches('*/master')
+                }
+            }
+            scriptPath('ci/Jenkinsfile')
+        }
+    }
+}
